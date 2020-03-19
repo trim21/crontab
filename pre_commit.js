09a080857edc696b33af2abe6af37e996c5b9b36
trim21 <i@trim21.me>
2020-03-19 08:38:37 +0800

    update pre-commit

--- pre_commit.js
+++ pre_commit.js
@@ -7,7 +7,7 @@ async function run () {
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = process.env.PERSONAL_GITHUB_TOKEN
+  const myToken = process.env.PAT
   const owner = process.env.REPO.split('/')[0]
   const repo = process.env.REPO.split('/')[1]
   const branch = process.env.BRANCH
