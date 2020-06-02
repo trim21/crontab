195eb5fd03229f268cf5b1616b20f43294aeb07a
Trim21 <i@trim21.me>
2020-06-02 09:12:38 +0800

    trim21/setup-poetry

--- common/version.sh
+++ common/version.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 echo "::set-output name=renovate::$(npm info renovate --json | jq -r .version)"
-echo "::set-output name=poetry::$(curl -sSl https://pypi.org/pypi/poetry/json | jq .info.version -r)"
 echo "::set-output name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
-
-echo "::add-path::$HOME/.local/bin"
-echo "::add-path::$HOME/.poetry/bin"
