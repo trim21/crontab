98fe7be84a6b774bbd237c0b622d2ae80cd6f2ed
Trim21 <i@trim21.me>
2020-06-02 15:43:50 +0800

    cache renovate cache

--- common/version.sh
+++ common/version.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo "::set-output name=renovate::$(npm info renovate --json | jq -r .version)"
 echo "::set-env name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
 
 echo "::add-path::$HOME/.local/bin"
