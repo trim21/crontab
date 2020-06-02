ec925e689cd39e4562556b07cb722683cad203bd
Trim21 <i@trim21.me>
2020-06-02 09:45:02 +0800

    use env

--- common/version.sh
+++ common/version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "::set-output name=renovate::$(npm info renovate --json | jq -r .version)"
-echo "::set-output name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
+echo "::set-env name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
 
 echo "::add-path::$HOME/.local/bin"
