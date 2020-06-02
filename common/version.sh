37af5cf65d686ea5060ba88a610d3e43c89d0e76
Trim21 <i@trim21.me>
2020-06-02 09:25:13 +0800

    fix

--- common/version.sh
+++ common/version.sh
@@ -2,3 +2,5 @@
 
 echo "::set-output name=renovate::$(npm info renovate --json | jq -r .version)"
 echo "::set-output name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
+
+echo "::add-path::$HOME/.local/bin"
