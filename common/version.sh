d062dfbc82be2233589fcd0466598134167461c6
Trim21 <i@trim21.me>
2020-10-14 04:37:50 +0800

    update

--- common/version.sh
+++ common/version.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo "::set-env name=PY::$(python -VV | md5sum | cut -d' ' -f1)"
-
+echo "PY=$(python -VV | md5sum | cut -d' ' -f1)" >>$GITHUB_ENV
 echo "$HOME/.local/bin" >>$GITHUB_PATH
