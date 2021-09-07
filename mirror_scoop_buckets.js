b491049324fed3492e41f1aefc45bd23f22a5308
Trim21 <trim21.me@gmail.com>
2021-09-08 02:46:14 +0800

    update

---
+++ mirror_scoop_buckets.js
@@ -0,0 +1,33 @@
+const exec = require('@actions/exec').exec
+const assert = require('assert')
+
+const path = require('path')
+const fs = require('fs')
+
+async function main () {
+  const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+  assert(ACCESS_TOKEN.length !== 0, 'no access token given')
+  const cwd = process.cwd()
+  const repos = {
+    'main': 'https://github.com/ScoopInstaller/Main',
+  }
+  for (const [repoName, url] of Object.entries(repos)) {
+    const repoDir = path.join(cwd, repoName)
+    if (fs.existsSync(repoDir)) {
+      fs.rmdirSync(repoDir, { recursive: true })
+    }
+    await exec(`git clone --mirror ${url} ${repoDir}`
+    )
+    process.chdir(repoDir)
+    await exec(
+      `git remote add --mirror=push gitea https://${ACCESS_TOKEN}@gitea.com/scoop-buckets/${repoName}.git`
+    )
+    await exec('git push gitea')
+
+  }
+}
+
+main().catch((e) => {
+  console.log(e)
+  process.exit(1)
+})
