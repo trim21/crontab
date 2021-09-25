612980db0bbef7ba0f17422bfef8bfe6117b99a5
Trim21 <trim21.me@gmail.com>
2021-09-26 05:24:56 +0800

    update

---
+++ common/date.py
@@ -0,0 +1,16 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+tz = timezone(timedelta(seconds=3600 * 8), "Asia/Shanghai")
+today = datetime.now(tz).date()
+
+ENV_FILE = os.getenv("GITHUB_ENV")
+
+with open(ENV_FILE, "a+", encoding="utf-8") as env_file:
+    print(f"TODAY={today}", file=env_file)
+    print(f"D1={today - timedelta(days=1)}", file=env_file)
+    print(f"D2={today - timedelta(days=2)}", file=env_file)
+
+    print(f"Y={today.year}", file=env_file)
+    print(f"M={today.month:02d}", file=env_file)
+    print(f"D={today.day:02d}", file=env_file)
