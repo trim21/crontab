b7b78684708f7aebdf147026a4d1eb1798e4788f
Trim21 <trim21.me@gmail.com>
2022-03-28 21:10:12 +0800

    update

--- common/date.js
+++ common/date.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const dayjs = require('dayjs')
 
-const ENV_FILE = process.env.GITHUB_ENV
+const ENV_FILE = process.env.GITHUB_ENV ?? './env_file'
 
 /**
  * @var {dayjs.Dayjs}
@@ -15,12 +15,12 @@ const today = new dayjs()
  * @return {string}
  */
 function toString(d) {
-  return `${d.year()}-${d.month() + 1}-${d.date()}`
+  return d.format("YYYY-MM-DD")
 }
 
-fs.appendFileSync(ENV_FILE, `Y=${today.year()}\n`)
-fs.appendFileSync(ENV_FILE, `M=${today.month() + 1}\n`)
-fs.appendFileSync(ENV_FILE, `D=${today.date()}\n`)
+fs.appendFileSync(ENV_FILE, `Y=${today.format('YYYY')}\n`)
+fs.appendFileSync(ENV_FILE, `M=${today.format('MM')}\n`)
+fs.appendFileSync(ENV_FILE, `D=${today.format('DD')}\n`)
 
 fs.appendFileSync(ENV_FILE, `TODAY=${toString(today)}\n`)
 fs.appendFileSync(ENV_FILE, `D1=${toString(today.subtract(1, 'd'))}\n`)
