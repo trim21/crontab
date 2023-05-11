a706e1c5aebef5529d89062e09aecdc7e07810ea
Trim21 <trim21.me@gmail.com>
2023-05-11 18:27:00 +0800

    update

--- common/date.mjs
+++ common/date.mjs
@@ -3,22 +3,22 @@ import * as core from '@actions/core';
 import dayjs from "dayjs";
 
 /**
- * @var {dayjs.Dayjs}
+ * @type {Date}
  */
-const today = new dayjs()
+const today = new Date()
 
 /**
  *
- * @param d {dayjs.Dayjs}
+ * @param d {Date}
  * @return {string}
  */
 function toString(d) {
-  return d.format("YYYY-MM-DD")
+  return d.toISOString().slice(0, 'yyyy-mm-dd'.length)
 }
 
-core.exportVariable('Y', today.format('YYYY'))
-core.exportVariable('M', today.format('MM'))
-core.exportVariable('D', today.format('DD'))
+core.exportVariable('Y', today.toISOString().slice(0, 4))
+core.exportVariable('M', today.toISOString().slice(5, 7))
+core.exportVariable('D', today.toISOString().slice(8, 10))
 core.exportVariable('TODAY', toString(today))
-core.exportVariable('D1', toString(today.subtract(1, 'd')))
-core.exportVariable('D2', toString(today.subtract(2, 'd')))
+core.exportVariable('D1', toString(new Date(today.getTime() + 1000 * 3600 * 24)))
+core.exportVariable('D2', toString(new Date(today.getTime() + 1000 * 3600 * 24 * 2)))
