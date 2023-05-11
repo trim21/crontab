8d3eb176083378540c42af60b532f8d42481407e
Trim21 <trim21.me@gmail.com>
2023-05-11 21:36:04 +0800

    update

--- common/date.mjs
+++ common/date.mjs
@@ -14,10 +14,14 @@ function toString(d) {
   return d.toISOString().slice(0, "yyyy-mm-dd".length);
 }
 
+let year = parseInt(today.toISOString().slice(0, 4), 10);
+let month = parseInt(today.toISOString().slice(5, 7), 10);
+
+core.exportVariable("TODAY", toString(today));
+
 core.exportVariable("Y", today.toISOString().slice(0, 4));
 core.exportVariable("M", today.toISOString().slice(5, 7));
 core.exportVariable("D", today.toISOString().slice(8, 10));
-core.exportVariable("TODAY", toString(today));
 
 [1, 2, 3].forEach((i) => {
   core.exportVariable(
@@ -25,3 +29,13 @@ core.exportVariable("TODAY", toString(today));
     toString(new Date(today.getTime() - i * 1000 * 3600 * 24))
   );
 });
+
+month = month - 1;
+
+if (month === 0) {
+  year = year - 1;
+  month = 12;
+}
+
+core.exportVariable("M_1", `${year}-${month.toString().padStart(2, "0")}`);
+core.exportVariable("Y_1", `${year}`);
