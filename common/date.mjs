fcd73e97b9d9c6878fc1650a6fe436c1c8f122c8
Trim21 <trim21.me@gmail.com>
2023-05-11 21:06:18 +0800

    update

--- common/date.mjs
+++ common/date.mjs
@@ -1,9 +1,9 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 
 /**
  * @type {Date}
  */
-const today = new Date()
+const today = new Date();
 
 /**
  *
@@ -11,12 +11,18 @@ const today = new Date()
  * @return {string}
  */
 function toString(d) {
-  return d.toISOString().slice(0, 'yyyy-mm-dd'.length)
+  return d.toISOString().slice(0, "yyyy-mm-dd".length);
 }
 
-core.exportVariable('Y', today.toISOString().slice(0, 4))
-core.exportVariable('M', today.toISOString().slice(5, 7))
-core.exportVariable('D', today.toISOString().slice(8, 10))
-core.exportVariable('TODAY', toString(today))
-core.exportVariable('D1', toString(new Date(today.getTime() + 1000 * 3600 * 24)))
-core.exportVariable('D2', toString(new Date(today.getTime() + 1000 * 3600 * 24 * 2)))
+core.exportVariable("Y", today.toISOString().slice(0, 4));
+core.exportVariable("M", today.toISOString().slice(5, 7));
+core.exportVariable("D", today.toISOString().slice(8, 10));
+core.exportVariable("TODAY", toString(today));
+core.exportVariable(
+  "D1",
+  toString(new Date(today.getTime() + 1000 * 3600 * 24))
+);
+core.exportVariable(
+  "D2",
+  toString(new Date(today.getTime() + 1000 * 3600 * 24 * 2))
+);
--- mirror_scoop_buckets.mjs
+++ mirror_scoop_buckets.mjs
@@ -2,10 +2,9 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import assert from "node:assert";
 
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 import { exec as _exec, getExecOutput } from "@actions/exec";
-import { simpleGit, CleanOptions } from 'simple-git';
-
+import { simpleGit, CleanOptions } from "simple-git";
 
 const cwd = process.cwd();
 const repoName = process.env.NAME;
@@ -13,33 +12,32 @@ const repoDir = path.join(cwd, "repos", repoName);
 const options = { cwd: repoDir };
 
 async function exec(cmd, args, opt) {
-  const buf = []
+  const buf = [];
 
   await _exec(cmd, args, {
     ...opt,
     listeners: {
       stderr(b) {
-        buf.push(b)
+        buf.push(b);
       },
       stdout(b) {
-        buf.push(b)
-      }
-    }
-  })
+        buf.push(b);
+      },
+    },
+  });
 
-  let out = `### ${cmd} ${args.join(' ')}\n`
+  let out = `### ${cmd} ${args.join(" ")}\n`;
   if (buf.length) {
     out += `
 \`\`\`\`\`\`text
 ${Buffer.concat(buf).toString().trim()}
 \`\`\`\`\`\`
 
-`
+`;
   }
 
-  return out
+  return out;
 }
-
 
 async function main() {
   const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
@@ -55,24 +53,25 @@ async function main() {
     out += await exec("git", ["fetch", "origin"], options);
   }
 
-  const repo = simpleGit(repoDir)
+  const repo = simpleGit(repoDir);
 
-  out += await exec("git", ["remote", "prune", "origin"], options)
+  out += await exec("git", ["remote", "prune", "origin"], options);
   out += await exec("git", ["fetch", "gitee"], options);
-  const oldHead = await repo.revparse('gitee/master')
+  const oldHead = await repo.revparse("gitee/master");
   out += await exec("git", ["reset", "--hard"], options);
   out += await exec("git", ["checkout", "master"], options);
   out += await exec("git", ["reset", "--hard", "origin/master"], options);
   out += await exec("git", ["push", "--force", "gitee", "master"], options);
-  const newHead = await repo.revparse('gitee/master')
+  const newHead = await repo.revparse("gitee/master");
   out += await exec("git", ["gc"], options);
 
   if (oldHead !== newHead) {
-    const logs = await getExecOutput(`git log ${oldHead}..${newHead} --graph --pretty=format:'%h%C(auto)%d %s <%an> %ad' --date=short-local`, null, options)
-    await core.summary
-      .addCodeBlock(logs.stdout)
-      .addRaw(out, true)
-      .write()
+    const logs = await getExecOutput(
+      `git log ${oldHead}..${newHead} --graph --pretty=format:'%h%C(auto)%d %s <%an> %ad' --date=short-local`,
+      null,
+      options
+    );
+    await core.summary.addCodeBlock(logs.stdout).addRaw(out, true).write();
   }
 }
 
