f9db9a732283f9c1f313c2d774932e7d19557541
Trim21 <trim21.me@gmail.com>
2023-05-17 01:36:12 +0800

    update

--- mirror_scoop_buckets.mjs
+++ mirror_scoop_buckets.mjs
@@ -61,11 +61,7 @@ async function main() {
   out += await exec("git", ["reset", "--hard"], options);
   out += await exec("git", ["checkout", "master"], options);
   out += await exec("git", ["reset", "--hard", "origin/master"], options);
-  out += await exec(
-    "git",
-    ["push", "--no-color", "--force", "gitee", "master"],
-    options
-  );
+  out += await exec("git", ["push", "--force", "gitee", "master"], options);
   const newHead = await repo.revparse("gitee/master");
   out += await exec("git", ["gc"], options);
 
